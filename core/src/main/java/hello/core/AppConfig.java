edbc65cc5e9b8d293e269b638a4f94f90def396c
Life is a ninanino <tvxqinak@naver.com>
2023-02-17 00:40:32 +0900

    스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 /**
@@ -16,21 +17,25 @@ import hello.core.order.OrderServiceImpl;
  * 생성한 객체 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다 -> 생성자 주입
  * 커맨트 + E = 히스토리
  */
+
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // 생성자 주입, 의존성 주입
     }
 
-
+    @Bean
     // command + option + M -> 리팩토링
-    private MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
